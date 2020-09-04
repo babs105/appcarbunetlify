93b8a03505cf53bde4e8db8d23d03d64c11dcde1
babs105 <mamembaye105@live.fr>
2020-09-04 21:03:53 +0000

    dist parcourue

--- src/components/Dashboard/components/ListRavitaillementCurrentMonth.js
+++ src/components/Dashboard/components/ListRavitaillementCurrentMonth.js
@@ -1,158 +1,168 @@
-import React from 'react';  
+import React from "react";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
-import { makeStyles } from '@material-ui/core/styles';  
-import Paper from '@material-ui/core/Paper';  
-import { Grid,TextField } from '@material-ui/core';
-import Table from '@material-ui/core/Table';  
-import TableBody from '@material-ui/core/TableBody';  
-import TableCell from '@material-ui/core/TableCell';  
-import TableContainer from '@material-ui/core/TableContainer';  
-import TableHead from '@material-ui/core/TableHead';  
-import TablePagination from '@material-ui/core/TablePagination';  
-import TableRow from '@material-ui/core/TableRow';    
-import Typography from '@material-ui/core/Typography';
-import {ravitailleService} from '../../../service/ravitailleService';
-import {ExportXlsx} from '../../Ravitaillement/ExportXlsx';
-import Loader from '../../loader/Loader';
-import { useState, useEffect } from 'react';
-  
-const useStyles = makeStyles({  
-  root: {  
-    width: '100%',  
-  },  
-  container: {  
-    maxHeight: 440,  
-  },  
-});  
-const style ={
-    display: 'flex',
-    justifyContent: 'center'
-}
-export default function MatPaginationTable() {  
-  const classes = useStyles();  
-  const [page, setPage] = React.useState(0);  
-  const [data, setData] = useState([]); 
-  const [loader,setLoader] = useState(false);  
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);  
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { Grid, TextField } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import { ravitailleService } from "../../../service/ravitailleService";
+import { ExportXlsx } from "../../Ravitaillement/ExportXlsx";
+import Loader from "../../loader/Loader";
+import { useState, useEffect } from "react";
 
-  useEffect(() => {    
-            ravitailleService.getAllOperationsCuveInCurrentMonth()
-            .then((res) => {
-                setData(res);
-                console.log("response",res);
-            });           
-        
-}, []);   
-  const handleChangePage = (event, newPage) => {  
-    setPage(newPage);  
-  };  
-  
-  const handleChangeRowsPerPage = event => {  
-    setRowsPerPage(+event.target.value);  
-    setPage(0);  
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+const style = {
+  display: "flex",
+  justifyContent: "center",
+};
+export default function MatPaginationTable() {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  useEffect(() => {
+    ravitailleService.getAllOperationsCuveInCurrentMonth().then((res) => {
+      setData(res);
+      console.log("response", res);
+    });
+  }, []);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
   };
   const onSearchInputChange = (event) => {
-    console.log("Search changed ..." + event.target.value)
+    console.log("Search changed ..." + event.target.value);
     if (event.target.value) {
-        // this.setState({searchString: event.target.value})
-        ravitailleService.searchRavitaillementByImmatricule(event.target.value)
+      // this.setState({searchString: event.target.value})
+      ravitailleService
+        .searchRavitaillementByImmatricule(event.target.value)
         .then((res) => {
-            console.log("result",res);
-            setData(res);
-            setLoader(false)
-            console.log("FOUND",data);  
-        }); 
-    } else {
-      setLoader(true)
-      ravitailleService.getAllOperationsCuveInCurrentMonth()
-      .then((res) => {
+          console.log("result", res);
           setData(res);
-          setLoader(false)
-          console.log(data);  
-      });  
-        
+          setLoader(false);
+          console.log("FOUND", data);
+        });
+    } else {
+      setLoader(true);
+      ravitailleService.getAllOperationsCuveInCurrentMonth().then((res) => {
+        setData(res);
+        setLoader(false);
+        console.log(data);
+      });
     }
-   
-};
-let i=0;
-  
-  return (  
-      <div>
-        
-    <Paper  style={{marginTop:'20px'}}className={classes.root}>  
-     
-            
-            <Typography variant="h5" style={{color:'green', display: 'flex',justifyContent:'center' ,paddingTop:'30px',marginBottom:'30px'}} >Ravitaillements du mois</Typography>
-            <Grid container alignItems="center" justify="center" >
-               <TextField style={{padding: 24}}
-                            id="searchInputNow"
-                            placeholder="Rechercher"   
-                            margin="normal"
-                            onChange={onSearchInputChange}
-                            InputProps={{
-                              endAdornment: (
-                                <InputAdornment>
-                                  <SearchIcon />
-                                </InputAdornment>
-                               )
-                              }}
-                            />
-                  <ExportXlsx csvData={data} fileName={"RapportRavitaillement"}/>
-           </Grid>
-      
-      <TableContainer className={classes.container}>  
-        <Table stickyHeader aria-label="sticky table">  
-        <TableHead>  
-        <TableRow>
-        <TableCell align="center">N°: </TableCell>
-            <TableCell align="center">DATE  </TableCell>
-            <TableCell align="center">QUANTITE RAVITAILLEE</TableCell>
-            {/* <TableCell align="center">ETAT CUVE </TableCell> */}
-            <TableCell align="center">IMMATRICULE</TableCell>
-            <TableCell align="center">KILOMETRAGE </TableCell>
-            <TableCell align="center">STATUT</TableCell>
-            <TableCell align="center">CONDUCTEUR</TableCell>
+  };
+  let i = 0;
 
-        </TableRow>
-          </TableHead>  
-          <TableBody>  
-            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {  
-              return (  
-                <TableRow key={row.id}>
-                  <TableCell align="center">{i=i+1}</TableCell>
-                <TableCell align="center" component="th" scope="row">
-                    {row.dateRavitaillement}
-                </TableCell>
-                <TableCell align="center">{row.quantityRavitaillement}</TableCell>
-                {/* <TableCell align="center">{row.quantityCurrentCuve}</TableCell> */}
-                <TableCell align="center">{row.immatricule}</TableCell>
-                
-                <TableCell align="center">{row.kilometrageCurrent}</TableCell>
-                <TableCell align="center">{row.vehicule.statut}</TableCell>
-                <TableCell align="center">{row.chauffeur}</TableCell>
-            
-          
-            </TableRow>
-                 
-              );  
-            })}  
-          </TableBody>  
-        </Table>  
-      </TableContainer>  
-      <TablePagination  
-        rowsPerPageOptions={[5, 10, 15]}  
-        component="div"  
-        count={data.length}  
-        rowsPerPage={rowsPerPage}  
-        page={page}  
-        onChangePage={handleChangePage}  
-        onChangeRowsPerPage={handleChangeRowsPerPage}  
-      />  
-    </Paper>  
+  return (
+    <div>
+      <Paper style={{ marginTop: "20px" }} className={classes.root}>
+        <Typography
+          variant="h5"
+          style={{
+            color: "green",
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "30px",
+            marginBottom: "30px",
+          }}
+        >
+          Ravitaillements du mois
+        </Typography>
+        <Grid container alignItems="center" justify="center">
+          <TextField
+            style={{ padding: 24 }}
+            id="searchInputNow"
+            placeholder="Rechercher"
+            margin="normal"
+            onChange={onSearchInputChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <ExportXlsx csvData={data} fileName={"RapportRavitaillement"} />
+        </Grid>
+
+        <TableContainer className={classes.container}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">N°: </TableCell>
+                <TableCell align="center">DATE </TableCell>
+                <TableCell align="center">QUANTITE RAVITAILLEE</TableCell>
+                {/* <TableCell align="center">ETAT CUVE </TableCell> */}
+                <TableCell align="center">IMMATRICULE</TableCell>
+                <TableCell align="center">KILOMETRAGE </TableCell>
+                <TableCell align="center">DIST.PARCOURUE</TableCell>
+                <TableCell align="center">STATUT</TableCell>
+                <TableCell align="center">CONDUCTEUR</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow key={row.id}>
+                      <TableCell align="center">{(i = i + 1)}</TableCell>
+                      <TableCell align="center" component="th" scope="row">
+                        {row.dateRavitaillement}
+                      </TableCell>
+                      <TableCell align="center">
+                        {row.quantityRavitaillement}
+                      </TableCell>
+                      {/* <TableCell align="center">{row.quantityCurrentCuve}</TableCell> */}
+                      <TableCell align="center">{row.immatricule}</TableCell>
+
+                      <TableCell align="center">
+                        {row.kilometrageCurrent}
+                      </TableCell>
+                      <TableCell align="center">
+                        {row.distanceParcourue}
+                      </TableCell>
+                      <TableCell align="center">
+                        {row.vehicule.statut}
+                      </TableCell>
+                      <TableCell align="center">{row.chauffeur}</TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 15]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
     </div>
-  );  
-} 
-
-
-    
+  );
+}
--- src/components/Dashboard/components/NumberRavitaillementCurrentMonth.js
+++ src/components/Dashboard/components/NumberRavitaillementCurrentMonth.js
@@ -1,78 +1,69 @@
-import React,{ useState, useEffect }  from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import Table from '@material-ui/core/Table';  
-import TableBody from '@material-ui/core/TableBody';  
-import TableCell from '@material-ui/core/TableCell';  
-import TableContainer from '@material-ui/core/TableContainer';  
-import TableHead from '@material-ui/core/TableHead';  
-import TablePagination from '@material-ui/core/TablePagination';  
-import TableRow from '@material-ui/core/TableRow';  
-import { makeStyles } from '@material-ui/core/styles';  
-import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import EqualizerIcon from '@material-ui/icons/Equalizer';
-import{ dashboardService} from '../../../service/dashboardService'
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardContent, Grid, Typography, Avatar } from "@material-ui/core";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import { dashboardService } from "../../../service/dashboardService";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100%'
+    height: "100%",
   },
   content: {
-    alignItems: 'center',
-    display: 'flex'
+    alignItems: "center",
+    display: "flex",
   },
   title: {
-    fontWeight: 700
+    fontWeight: 700,
   },
   avatar: {
     backgroundColor: theme.palette.success.main,
     height: 56,
-    width: 56
+    width: 56,
   },
   icon: {
     height: 32,
-    width: 32
+    width: 32,
   },
   difference: {
     marginTop: theme.spacing(2),
-    display: 'flex',
-    alignItems: 'center'
+    display: "flex",
+    alignItems: "center",
   },
   differenceIcon: {
-    color: theme.palette.error.dark
+    color: theme.palette.error.dark,
   },
   differenceValue: {
     color: theme.palette.error.dark,
-    marginRight: theme.spacing(1)
-  }
+    marginRight: theme.spacing(1),
+  },
 }));
 
 export default function NumberRavitaillementAndTotalQteCurrentMonth(props) {
   const { className, ...rest } = props;
 
   const classes = useStyles();
-  const [data, setData] = useState([]);   
-  useEffect(() => {    
-            dashboardService.getRavitaillementByVehiculeInCurrentMonth()
-            .then((res) => {
-                setData(res);
-                
-            });           
-        
-}, []); 
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    dashboardService.getRavitaillementByVehiculeInCurrentMonth().then((res) => {
+      setData(res);
+    });
+  }, []);
 
-let i=0;
+  let i = 0;
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
+    <Card {...rest} className={clsx(classes.root, className)}>
       <CardContent>
-        <Grid
-          container
-          justify="space-between"
-        >
+        <Grid container justify="space-between">
           <Grid item>
             <Typography
               className={classes.title}
@@ -82,7 +73,6 @@ let i=0;
             >
               Nombre de Ravitaillement/Quantité Totale par Véhicule du mois
             </Typography>
-           
           </Grid>
           <Grid item>
             <Avatar className={classes.avatar}>
@@ -91,43 +81,43 @@ let i=0;
           </Grid>
         </Grid>
 
-        <TableContainer className={classes.container}>  
-        <Table >  
-        <TableHead>  
-        <TableRow>
-        {/* <TableCell align="center">N°: </TableCell> */}
-            <TableCell align="center">IMMATRICULE  </TableCell>
-            <TableCell align="center">QUANTITE RAVITAILLEE</TableCell>
-            <TableCell align="center">NOMBRE RAVITAILLEMENT</TableCell>
-            <TableCell align="center">DISTANCE PARCOURUE</TableCell>
-          
-        </TableRow>
-          </TableHead>  
-          <TableBody>  
-            {data.map(row => {  
-              return (  
-                <TableRow key={i=i+1}>
-                  {/* <TableCell align="center">{i=i+1}</TableCell> */}
-                  <TableCell align="center" component="th" scope="row">
-                    {row.immatricule}
-                  </TableCell>
-                  <TableCell align="center">{row.totalQteRavitaillement}</TableCell>
-                  <TableCell align="center">{row.nombreRavitaillement}</TableCell>
-                  <TableCell align="center">{row.maxKilometrage - row.minKilometrage}</TableCell>
-               </TableRow>
-                 
-              );  
-            })}  
-          </TableBody>  
-        </Table>  
-      </TableContainer>  
+        <TableContainer className={classes.container}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {/* <TableCell align="center">N°: </TableCell> */}
+                <TableCell align="center">IMMATRICULE </TableCell>
+                <TableCell align="center">QUANTITE RAVITAILLEE</TableCell>
+                <TableCell align="center">NOMBRE RAVITAILLEMENT</TableCell>
+                {/* <TableCell align="center">DISTANCE PARCOURUE</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((row) => {
+                return (
+                  <TableRow key={(i = i + 1)}>
+                    {/* <TableCell align="center">{i=i+1}</TableCell> */}
+                    <TableCell align="center" component="th" scope="row">
+                      {row.immatricule}
+                    </TableCell>
+                    <TableCell align="center">
+                      {row.totalQteRavitaillement}
+                    </TableCell>
+                    <TableCell align="center">
+                      {row.nombreRavitaillement}
+                    </TableCell>
+                    {/* <TableCell align="center">{row.maxKilometrage - row.minKilometrage}</TableCell> */}
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </CardContent>
     </Card>
   );
-};
+}
 
 NumberRavitaillementAndTotalQteCurrentMonth.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
 };
-
-
