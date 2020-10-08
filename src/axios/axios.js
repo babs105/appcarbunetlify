7248257b06e5979d9e8cc0a3f992b16af1634070
babs105 <mamembaye105@live.fr>
2020-10-08 13:39:00 +0000

    deploy to aws 1

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,5 +3,6 @@ import axios from "axios";
 export default axios.create({
   //baseURL: "http://localhost:8080",
   //baseURL: 'https://appcarburant.herokuapp.com'
-  baseURL: "AppCarburant-env.eba-dft9jnbm.us-west-2.elasticbeanstalk.com", //EBS(Elastic Beanstalk) aws
+  baseURL:
+    "http://appcarburant-env.eba-dft9jnbm.us-west-2.elasticbeanstalk.com", //EBS(Elastic Beanstalk) aws
 });
